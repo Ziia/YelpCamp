b6242480447ebe673f5a3e2e0109484675ae60f5
Ziia <Ziarobin@gmail.com>
2017-01-25 16:17:19 +0100

    Fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var passport = require("passport");
 var bodyParser = require("body-parser");
 var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 // Require modules
@@ -24,6 +25,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 app.use(require("express-session")({
   secret: "July 2017",
   resave: false,
